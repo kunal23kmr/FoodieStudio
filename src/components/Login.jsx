858aa23cf1be3e5839696e042557ecb957bc4266
kunal <kunal23kmr@gmail.com>
2023-10-14 11:29:58 +0530

    Login Page Done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
+import './Login.css';
 
-function Login(){
-    return <>
-    <h2>Login</h2>
-    </>
+function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <form className="login-form">
+                <input type="number" placeholder="Phone Number" inputMode="numeric" />
+
+                <div className="password-container">
+                    <input
+                        className='password'
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                    />
+                    <span
+                        className={`eye-icon ${showPassword ? "show" : ""}`}
+                        onClick={togglePasswordVisibility}
+                    >show
+                    </span>
+                </div>
+                <button type='submit' className="login-button">Login</button>
+            </form>
+        </div>
+    );
 }
+
 export default Login;
