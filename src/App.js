2335221e2e6495b135319f7ec778ce5e38775ac7
keerti15singh <keertisinghstudya1@gmail.com>
2023-10-16 02:14:24 +0530

    About Page

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
       <Login />
       {/* <Signup />
       <Blog />
-      <Contact />
-      <About /> */}
+      <Contact />*/}
+      <About /> 
     </>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React from 'react';
+import './About.css';
 
 function About() {
-    return <>
-        <h2>About</h2>
-    </>
+  return (
+    <div className="about-page">
+      <div className="about-content">
+        <h1>About Us</h1>
+        <p>
+          Welcome to our food ordering and delivery service. We are dedicated to bringing you the best dining experience at your doorstep. Our mission is to provide high-quality, delicious food with fast and reliable delivery.
+        </p>
+        <p>
+          Whether you're looking for a quick lunch, a family dinner, or catering for a special event, we have a wide range of menu options to satisfy your cravings.
+        </p>
+        <p>
+          Thank you for choosing us. We look forward to serving you and making your dining experience extraordinary.
+        </p>
+      </div>
+      <div className="about-buttons">
+        <button className="contact-button">Contact Us</button>
+        <button className="order-button">Order Now</button>
+      </div>
+    </div>
+  );
 }
+
 export default About;
