eea03195e8592b95b92976586acc0edfcfaa5a58
keerti15singh <keertisinghstudya1@gmail.com>
2023-10-16 16:54:07 +0530

    Again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <div className="about-content">
         <h1>About Us</h1>
         <p>
-          Welcome to our food ordering and delivery service. We are dedicated to bringing you the best dining experience at your doorstep. Our mission is to provide high-quality, delicious food with fast and reliable delivery.
+          Welcome to our food ordering and delivery service. <br/>We are dedicated to bringing you the best dining experience at your doorstep. Our mission is to provide high-quality, delicious food with fast and reliable delivery.
         </p>
         <p>
           Whether you're looking for a quick lunch, a family dinner, or catering for a special event, we have a wide range of menu options to satisfy your cravings.
