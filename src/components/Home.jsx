dfac36d0dea58441bc411ee04537eb7189c06e82
keerti15singh <keertisinghstudya1@gmail.com>
2023-10-17 11:39:16 +0530

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,59 @@
 import React from "react";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import '../css/Home.css';
+import axios from 'axios'
 
+const foodItems = [
+    {
+      id: 1,
+      title: 'Rasgulla',
+      description: 'Description of Food Item 1.',
+      restaurant: 'Kunal da Dhaba',
+      image: 'D:/FoodieStudio/FoodieStudio/src/images/food-images/food1.jpg',
+      price: '$10',
+    },
+    
+    {
+        id: 2,
+        title: 'Cholle Bhature',
+        description: 'Description of Food Item 2.',
+        restaurant: 'Swadeshi Bhog',
+        image: 'scr/images/food-images/food2.jpg',
+        price: '$20',  
+    }
+  ];
 function Home() {
-    return <>
-        <h2>Home</h2>
-    </>
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const showDetails = (item) => {
+      setSelectedItem(item);
+    };
+    return(
+        <div className="App">
+          <h1>Welcome to FoodieStudio</h1><br/><br/>
+          <div className="food-cards">
+            {foodItems.map((item) => (
+              <div key={item.id} className="food-card">
+                <img src="FoodieStudio\src\images\food-images\food1.jpg" alt={item.title} />
+                <h2>{item.title}</h2>
+                <p>By: {item.restaurant}</p>
+                <button onClick={() => showDetails(item)}>View Details</button>
+              </div>
+            ))}
+          </div>
+    
+          {selectedItem && (
+            <div className="details">
+              <h2>{selectedItem.title}</h2>
+              <h3>{selectedItem.image}</h3>
+              <p>{selectedItem.description}</p>
+              <p>Restaurant: {selectedItem.restaurant}</p>
+              <p>Price: {selectedItem.price}</p>
+              <button className="close-button" onClick={() => setSelectedItem(null)}>Close Details</button>
+            </div>
+          )}
+        </div>
+      );
 }
 export default Home;
