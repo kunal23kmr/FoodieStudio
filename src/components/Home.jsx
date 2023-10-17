49263ec9225a157e28f73fccb9f2fa6ff72dd69a
keerti15singh <keertisinghstudya1@gmail.com>
2023-10-17 14:13:26 +0530

    lab

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,58 +2,127 @@ import React from "react";
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../css/Home.css';
-import axios from 'axios'
+import axios from 'axios';
+import image1 from '../images/food_images/food1.jpg';
 
 const foodItems = [
-    {
-      id: 1,
-      title: 'Rasgulla',
-      description: 'Description of Food Item 1.',
-      restaurant: 'Kunal da Dhaba',
-      image: 'D:/FoodieStudio/FoodieStudio/src/images/food-images/food1.jpg',
-      price: '$10',
-    },
-    
-    {
-        id: 2,
-        title: 'Cholle Bhature',
-        description: 'Description of Food Item 2.',
-        restaurant: 'Swadeshi Bhog',
-        image: 'scr/images/food-images/food2.jpg',
-        price: '$20',  
-    }
-  ];
-function Home() {
-    const [selectedItem, setSelectedItem] = useState(null);
+  {
+    id: 1,
+    title: 'Rasgulla',
+    description: 'Description of Food Item 1.',
+    restaurant: 'Kunal da Dhaba',
+    image: image1,
+    price: '$10',
+  },
 
-    const showDetails = (item) => {
-      setSelectedItem(item);
-    };
-    return(
-        <div className="App">
-          <h1>Welcome to FoodieStudio</h1><br/><br/>
-          <div className="food-cards">
-            {foodItems.map((item) => (
-              <div key={item.id} className="food-card">
-                <img src="FoodieStudio\src\images\food-images\food1.jpg" alt={item.title} />
-                <h2>{item.title}</h2>
-                <p>By: {item.restaurant}</p>
-                <button onClick={() => showDetails(item)}>View Details</button>
-              </div>
-            ))}
+  {
+    id: 2,
+    title: 'Cholle Bhature',
+    description: 'Description of Food Item 2.',
+    restaurant: 'Swadeshi Bhog',
+    image: image1,
+    price: '$20',
+  },
+  {
+    id: 3,
+    title: 'Dhokla',
+    description: 'Description of Food Item 3.',
+    restaurant: 'Swadeshi Bhog',
+    image: image1,
+    price: '$20',
+  },
+  {
+    id: 4,
+    title: 'Manchurian',
+    description: 'Description of Food Item 4.',
+    restaurant: 'Fast Foodies',
+    image: image1,
+    price: '$20',
+  },
+  {
+    id: 5,
+    title: 'Paneer Butter Masala',
+    description: 'Description of Food Item 5.',
+    restaurant: 'Kunal da Dhaba',
+    image: image1,
+    price: '$20',
+  },
+  {
+    id: 6,
+    title: 'Butter Naan',
+    description: 'Description of Food Item 6.',
+    restaurant: 'Swadeshi Bhog',
+    image: image1,
+    price: '$20',
+  },
+  {
+    id: 7,
+    title: 'Spring Roll',
+    description: 'Description of Food Item 7.',
+    restaurant: 'Fast Foodies',
+    image: image1,
+    price: '$20',
+  },
+  {
+    id: 8,
+    title: 'MOMOS',
+    description: 'Description of Food Item 8.',
+    restaurant: 'Swadeshi Bhog',
+    image: image1,
+    price: '$20',
+  },
+  {
+    id: 9,
+    title: 'Amritsari Kulcha',
+    description: 'Description of Food Item 9.',
+    restaurant: 'Swadeshi Bhog',
+    image: image1,
+    price: '$20',
+  },
+  {
+    id: 10,
+    title: 'Dosa',
+    description: 'Description of Food Item 10.',
+    restaurant: 'Southie Eats',
+    image: image1,
+    price: '$20',
+  }
+];
+
+function Home() {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const showDetails = (item) => {
+    setSelectedItem(item);
+  };
+
+  return (
+    <div className="App">
+      <h1>Welcome to FoodieStudio</h1><br /><br />
+      <h2> <u>Browse your Favorite food from your Favorite Restaurants</u> </h2><br />
+      <div className="food-cards">
+        {foodItems.map((item) => (
+          <div key={item.id} className="food-card">
+            <img src={item.image} alt={item.title} />
+            <h3>{item.title}</h3>
+            <p>By: {item.restaurant}</p>
+            <button onClick={() => showDetails(item)}>View Details</button>
           </div>
-    
-          {selectedItem && (
-            <div className="details">
-              <h2>{selectedItem.title}</h2>
-              <h3>{selectedItem.image}</h3>
-              <p>{selectedItem.description}</p>
-              <p>Restaurant: {selectedItem.restaurant}</p>
-              <p>Price: {selectedItem.price}</p>
-              <button className="close-button" onClick={() => setSelectedItem(null)}>Close Details</button>
-            </div>
-          )}
+        ))}
+      </div>
+
+      {selectedItem && (
+        <div className="details">
+          <h2>{selectedItem.title}</h2>
+          <img src={selectedItem.image} alt={selectedItem.title} />
+          <p>{selectedItem.description}</p>
+          <p>Restaurant: {selectedItem.restaurant}</p>
+          <p>Price: {selectedItem.price}</p>
+          <button className="close-button" onClick={() => setSelectedItem(null)}>Close Details</button>
         </div>
-      );
+      )}
+    </div>
+  );
 }
+
 export default Home;
